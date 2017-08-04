d890aefcf31e046a77c592ce7a6868cba6b6849f
Johniel <johniel.s.m@gmail.com>
2017-08-04 23:25:03 +0900

    uniquify

--- maps/parse.py
+++ maps/parse.py
@@ -3,9 +3,14 @@
 import sys
 from html.parser import HTMLParser
 
+paths = set()
 for line in sys.stdin.readlines():
     begin = line.find("?map=/map")
     if begin != -1:
         end = line[begin:].find('.json')
         if end != -1:
-            print("http://punter.inf.ed.ac.uk" + line[begin+5:begin+end] + ".json")
+            path = "http://punter.inf.ed.ac.uk" + line[begin+5:begin+end] + ".json"
+            paths.add(path)
+
+for path in paths:
+    print(path)
