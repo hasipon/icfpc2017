52c3bc1a7891187e9b4820b97e11395e92b9a6d5
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2017-08-04 23:57:19 +0900

    move 追加

--- shiota/test.cpp
+++ shiota/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
-#include "../common/include/Graph.h"
+#include "../common/include/Graph.hpp"
+#include "../common/include/Move.hpp"
 
 using namespace std;
 
@@ -8,4 +9,7 @@ int main(){
     Graph g;
     cout << g.edges.size() << endl;
     cout << g.mines.size() <<endl;
+    Move m = (Move){0, 0, 0, 0};
+    cout << m.source << endl;
+    cout << m.punter_id << endl;
 }
