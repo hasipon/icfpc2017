1ac286d88f8c51251729c24395e1394050e0901f
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2017-08-05 03:28:46 +0900

    scriptの場所を変更

---
+++ AIs/shiota_ai/ai.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+struct AI {
+	void Init(int punter_id, int num_of_punters, const Graph& g) {
+		
+	}
+	Move Think(const Moves& moves) {
+		return Move();
+	}
+};
---
+++ common/script/server_connecter.rb
@@ -0,0 +1,100 @@
+require "socket"
+require 'optparse'
+require 'pp'
+
+class Client
+  def initialize(host, port, ai_path)
+    exit 1 unless @server_socket = TCPSocket.new(host, port)
+    exit 1 unless @ai_socket = IO.popen(ai_path, "r+")
+  end
+
+  def handshake(name)
+    msg = '{"me":"'+ "#{name}@#{Time.now.to_i}" + '"}'
+    send_msg_to_server("#{msg.length}:#{msg}")
+    next_server_message
+  end
+
+  def next_server_message
+    log "wait server message"
+    ret = receive_messege(server_socket)
+    log "me <- server\n#{ret}"
+    ret
+  end
+
+  def next_ai_message
+    log "wait ai message"
+    ret = receive_messege(ai_socket)
+    # log "ai -> me\n#{ret}"
+    ret
+  end
+
+  def send_msg_to_ai(msg)
+    # log "ai <- me\n#{msg}"
+    ai_socket.print(msg)
+  end
+
+  def send_msg_to_server(msg)
+    log "me -> server\n#{msg}"
+    server_socket.print(msg)
+  end
+
+  def receive_messege(stream)
+    len = 0 
+    while c = stream.getc
+      break if c == ":"
+      len *= 10
+      len += c.to_i
+    end
+    return nil if len == 0
+    msg = stream.read(len)
+    "#{len}:#{msg}"
+  end
+
+  def server_socket
+    @server_socket
+  end
+
+  def ai_socket
+    @ai_socket
+  end
+
+  def log(msg)
+    STDERR.puts(msg)
+  end
+
+end
+
+
+
+port =  nil
+ai_path = nil
+host = nil
+name = "bob"
+opt = OptionParser.new
+opt.on('-p', '--port PORT') {|v| port =  v }
+opt.on('-h', '--host HOST') {|v| host =  v }
+opt.on('-a', '--ai AI_PATH') {|v| ai_path =  v }
+opt.on('-n', '--name NAME') {|v| name =  v }
+opt.parse(ARGV)
+
+client = Client.new(host, port.to_i, ai_path)
+
+# handshake
+client.handshake name
+
+# setup
+setup_msg = client.next_server_message
+client.send_msg_to_ai(setup_msg)
+
+# ready
+# client.send_msg_to_server '11:{"ready":0}'
+ready_msg = client.next_ai_message
+client.send_msg_to_server ready_msg
+
+while msg = client.next_server_message
+  client.send_msg_to_ai msg
+  msg = client.next_ai_message
+  break unless msg
+  client.send_msg_to_server msg
+end
+
