23a92d15028e8a236b70f35ecab3aa973bbd2ce9
Shingo Inada <inada-s@klab.com>
2017-08-04 17:24:16 +0900

    add gv.hpp

--- inada-s/src/main.cpp
+++ inada-s/src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <glog/logging.h>
+
+#define GV_JS
+#include "gv.hpp"
 using namespace std;
 
 int main(int argc, char** argv) { 
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
+
+  g_gvDefaultAlpha = 180;
+
+  for (int i = 0; i < 100; ++i) {
+    gvLine(0, i, 1, 1, gvColor(i));
+    gvLine(0, 0, i, 1);
+    gvLine(4, 2, 2, 3);
+    gvNewTime();
+  }
   cout << "hello" << endl;
 }
