247ae4c18c8b7de10da5a4070eef0b967a14c4e9
Johniel <johniel.s.m@gmail.com>
2017-08-05 15:23:06 +0900

    multiplay

--- match.sh
+++ match.sh
@@ -3,14 +3,15 @@
 set -eu
 
 function usage(){
-    echo match.sh PORT MAP-NAME AI-NAME1 AI-NAME2
+    echo match.sh PORT MAP-NAME AI-NAME1 AI-NAME2 NUM_PENTER
 }
 PORT=$1
 MAP=$2
 AI1=$3
 AI2=$4
+NUM_PENTER=$5
 
-ruby ./common/server/app.rb -n 2 -p $PORT -m ./maps/$MAP.json &
+ruby ./common/server/app.rb -n $5 -p $PORT -m ./maps/$MAP.json &
 sleep 1
 
 pushd ./AIs/$AI1/
@@ -21,6 +22,16 @@ pushd ./AIs/$AI2/
 make
 popd
 
-cd ./dashboard/static
+pushd ./dashboard/static
 ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$AI1/a.out -h localhost -n $AI1 &
 ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$AI2/a.out -h localhost -n $AI2 &
+popd
+
+RANDOM_AI=shiota_ai
+pushd ./AIs/$RANDOM_AI/
+make
+popd
+pushd ./dashboard/static
+for ((i=2; i < $NUM_PENTER; i++)); do
+    ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$RANDOM_AI/a.out -h localhost -n ${RANDOM_AI}-$i &
+done
