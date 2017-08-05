f23e90eab0decee7d8904543c3280edead4a0535
Johniel <johniel.s.m@gmail.com>
2017-08-05 15:47:47 +0900

    multiplay again

--- match.sh
+++ match.sh
@@ -3,35 +3,26 @@
 set -eu
 
 function usage(){
-    echo match.sh PORT MAP-NAME AI-NAME1 AI-NAME2 NUM_PENTER
+    echo match.sh PORT MAP-NAME AI-NAME1 AI-NAME2 
 }
 PORT=$1
 MAP=$2
-AI1=$3
-AI2=$4
-NUM_PENTER=$5
 
-ruby ./common/server/app.rb -n $5 -p $PORT -m ./maps/$MAP.json &
+ARGS=("$@")
+LOG_OPTION="-q"
+NUM_ARGS=${#ARGS[@]}
+
+ruby ./common/server/app.rb -n $(( $NUM_ARGS - 2 )) -p $PORT -m ./maps/$MAP.json &
 sleep 1
 
-pushd ./AIs/$AI1/
-make
-popd
-
-pushd ./AIs/$AI2/
-make
-popd
-
-pushd ./dashboard/static
-ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$AI1/a.out -h localhost -n $AI1 &
-ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$AI2/a.out -h localhost -n $AI2 -q &
-popd
-
-RANDOM_AI=shiota_ai
-pushd ./AIs/$RANDOM_AI/
-make
-popd
-pushd ./dashboard/static
-for ((i=2; i < $NUM_PENTER; i++)); do
-    ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$RANDOM_AI/a.out -h localhost -n ${RANDOM_AI}-$i -q &
+for ((i=2; i < $NUM_ARGS; i++)); do
+    AI_NAME=${ARGS[i]}
+    echo $AI_NAME
+    pushd ./AIs/$AI_NAME/
+    make
+    popd
+    pushd ./dashboard/static
+    ruby ../../common/script/server_connecter.rb -p $PORT -a ../../AIs/$AI_NAME/a.out -h localhost -n $AI_NAME &
+    popd
+    LOG_OPTION=""
 done
