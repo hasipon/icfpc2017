fcfdb6f20f15a42a3f9fca41249e3760307bba22
shohei909 <909mmsphere@gmail.com>
2017-08-07 01:06:09 +0900

    パスのみするAI

--- AI3/sawa_rust/src/main.rs
+++ AI3/sawa_rust/src/main.rs
@@ -18,8 +18,24 @@ fn main()
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    write_json_message(&mut stdout, serde_json::to_string(&mut Me{me: "shohei909".to_owned()}).unwrap());
+    write_json_message(&mut stdout, serde_json::to_string(&Me{me: "sawa_rust".to_owned()}).unwrap());
     read_json_message(&mut stdin);
+    let value = read_json_message(&mut stdin);
+    
+    if let Value::Object(mut object) = value
+    {
+        if object.contains_key("punters") {
+            let value = Value::Object(object);
+            let message:SetupMessage = serde_json::from_value(value).unwrap();
+            let punter = message.punter;
+            let state = setup(message);
+            write_json_message(&mut stdout, serde_json::to_string(&ReadyMessage{ready:punter, state:state}).unwrap());
+        } else if let Option::Some(value) = object.remove("move") {
+            let message:MovesMessage = serde_json::from_value(value).unwrap();
+            let mov = think(message);
+            write_json_message(&mut stdout, serde_json::to_string(&mov).unwrap());
+        }
+    }
 }
 
 fn read_json_message(input: &mut Read) -> Value
@@ -56,14 +72,114 @@ fn write_json_message(output: &mut Write, string:String) {
     output.flush();
 }
 
-struct PunterId(i32);
-struct SiteId(i32);
-struct EdgeId(i32);
-
-struct Edge {}
-
 // MESSAGE
 #[derive(Serialize, Deserialize)]
 struct Me { me : String }
 #[derive(Serialize, Deserialize)]
 struct You { you : String }
+#[derive(Serialize, Deserialize)]
+pub struct SetupMessage {
+    punter: PunterId,
+    punters: i64,
+    map: MapMessage,
+}
+#[derive(Serialize, Deserialize)]
+struct MapMessage {
+    sites: Vec<SiteMessage>,
+    rivers: Vec<RiverMessage>,
+    mines: Vec<SiteId>,
+}
+#[derive(Serialize, Deserialize)]
+struct SiteMessage {
+    id: SiteId,
+}
+#[derive(Serialize, Deserialize)]
+struct RiverMessage {
+    source: SiteId,
+    target: SiteId,
+}
+#[derive(Serialize, Deserialize)]
+struct ReadyMessage {
+    ready: PunterId,
+    state: GameState,
+}
+#[derive(Serialize, Deserialize)]
+struct GameState {
+    punter: PunterId,
+}
+#[derive(Serialize, Deserialize)]
+struct MovesMessage {
+    moves: Vec<MoveMessage>,
+    state: GameState,
+}
+#[derive(Serialize, Deserialize)]
+struct MoveMessage {
+    claim : Option<ClaimMessage>,
+    pass : Option<PassMessage>, 
+    splurge : Option<SplugeMessage>,
+    state : Option<GameState>,
+}
+#[derive(Serialize, Deserialize)]
+struct PassMessage {
+    punter: PunterId,
+}
+#[derive(Serialize, Deserialize)]
+struct ClaimMessage {
+    punter: PunterId,
+    source: SiteId, 
+    target: SiteId,
+}
+#[derive(Serialize, Deserialize)]
+struct SplugeMessage {
+    punter: PunterId,
+    route: Vec<SiteId>, 
+}
+
+#[derive(Serialize, Deserialize, Copy, Clone)]
+struct PunterId(i32);
+#[derive(Serialize, Deserialize, Copy, Clone)]
+struct SiteId(i32);
+#[derive(Serialize, Deserialize, Copy, Clone)]
+struct EdgeId(i32);
+
+enum Move {
+    Claim(SiteId, SiteId),
+    Pass,
+}
+
+impl Move {
+    fn to_message(self, id:PunterId, state:GameState) -> MoveMessage {
+        match self {
+            Move::Claim(a, b) => MoveMessage{
+                claim : Option::Some(ClaimMessage {
+                    punter: id,
+                    source: a,
+                    target: b,
+                }),
+                pass : Option::None, 
+                splurge : Option::None,
+                state : Option::Some(state),
+            },
+            Move::Pass => MoveMessage{
+                claim : Option::None, 
+                pass : Option::Some(PassMessage {
+                    punter: id,
+                }), 
+                splurge : Option::None,
+                state : Option::Some(state),
+            },
+        }
+    }
+}
+
+// AI
+fn setup(message:SetupMessage)->GameState
+{
+    return GameState{
+        punter: message.punter,
+    }
+}
+fn think(message:MovesMessage)->MoveMessage
+{
+    return Move::Pass.to_message(message.state.punter, message.state);
+}
