465dd2c0640e0cc53870e4046e34c2803509cf18
hasi <takuya.hashimoto@gmail.com>
2017-08-06 10:39:28 +0000

    modify hasi_template3

--- common/hasi_template3/common.hpp
+++ common/hasi_template3/common.hpp
@@ -10,12 +10,9 @@ using Edge = pair<int, int>;
 
 struct Move {
     int punter_id;
-    int source;
-    int target;
-    bool is_pass;
-    Move() : is_pass(true) {}
-    Move(int source, int target) : is_pass(false), source(source), target(target) {}
-    Move(Edge e) : is_pass(false), source(e.first), target(e.second) {}
+    vector<int> route;
+    Move() {}
+    Move(int punter_id, const vector<int>& route) : punter_id(punter_id), route(route) {}
 };
 
 using Moves = vector<Move>;
